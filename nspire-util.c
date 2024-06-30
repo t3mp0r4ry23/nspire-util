aba1193a1e3b639807b1539da8c311099709f1e4
t3mp0r4ry <80374341+t3mp0r4ry23@users.noreply.github.com>
2024-06-29 22:51:11 -0700

    compiles now

--- nspire-util.c
+++ nspire-util.c
@@ -5,6 +5,9 @@
 
 nspire_handle_t *handle;
 struct nspire_devinfo *device_info;
+struct nspire_dir_item *file_info;
+FILE * nspire_file;
+char * nspire_file_buffer;
 int ret = 0;
 int cmd = -1;
 const char * cmds[] = {"storage", "ram", "power", "lcd", "send", "get", "rm", "touch", "ls", "mkdir"};
@@ -35,12 +38,7 @@ int main(int argc, char * argv[]) {
 	}
 
 	if (cmd > 3 && cmd < 8) {
-		struct nspire_dir_item *file_info;
 		file_info = malloc(sizeof(struct nspire_dir_item));
-		if (cmd < 6) {
-			FILE * nspire_file;
-			char * nspire_file_buffer;
-		}
 	}
 
 	switch(cmd) {
@@ -56,7 +54,7 @@ int main(int argc, char * argv[]) {
 			break;
 		case 1:
 			printf("%.2lf MiB of RAM used out of %.2lf MiB, leaving %.2lf MiB free.\n",
-				(float) (device_info->ram.total - info->ram.free)/(1024*1024),
+				(float) (device_info->ram.total - device_info->ram.free)/(1024*1024),
 				(float) device_info->ram.total/(1024*1024),
 				(float) device_info->ram.free/(1024*1024));
 			break;
@@ -84,16 +82,16 @@ int main(int argc, char * argv[]) {
 			}
 
 			fseek(nspire_file, 0, SEEK_END);
-			long nspire_file_size = ftell(send_file);
+			long nspire_file_size = ftell(nspire_file);
 			fseek(nspire_file, 0, SEEK_SET);
 
 			printf("Reading data from host from file at %s...\n", argv[2]);
-			char * nspire_file_buffer = malloc(send_file_size + 1);
-			fread(nspire_file_buffer, send_file_size, 1, send_file);
+			char * nspire_file_buffer = malloc(nspire_file_size + 1);
+			fread(nspire_file_buffer, nspire_file_size, 1, nspire_file);
 			fclose(nspire_file);
 
-			printf("Sending %.3f %s of data to calculator to save to file at %s...\n", nspire_file_size > 999999 ? ((float) send_file_size/(1024*1024)) : ((float) send_file_size/1024), send_file_size > 999999 ? "MiB" : "KiB", argv[3]);
-			ret = nspire_file_write(handle, argv[3], nspire_file_buffer, send_file_size);
+			printf("Sending %.3f %s of data to calculator to save to file at %s...\n", nspire_file_size > 999999 ? ((float) nspire_file_size/(1024*1024)) : ((float) nspire_file_size/1024), nspire_file_size > 999999 ? "MiB" : "KiB", argv[3]);
+			ret = nspire_file_write(handle, argv[3], nspire_file_buffer, nspire_file_size);
 			if (ret) {
 				fprintf(stderr, "Error while writing: %s\n", nspire_strerror(ret));
 				break;
@@ -124,12 +122,12 @@ int main(int argc, char * argv[]) {
 			nspire_file_buffer = malloc(file_info->size + 1);
 
 			printf("Reading %.3f %s of data from %s...\n",
-				file_info->size > 999999 ? ((float) file_info->size/(1024*1024)) :
-					file_info->size > 999 ? ((float) file_info->size/1024) :
-						(float) file_info->size),
+				file_info->size > 999999 ? (float) file_info->size/(1024*1024) :
+				file_info->size > 999 ? ((float) file_info->size/1024) :
+				((float) file_info->size),
 				file_info->size > 999999 ? "MiB" :
-					file_info->size > 999 ? "KiB" :
-						"bytes",
+				file_info->size > 999 ? "KiB" :
+				"bytes",
 				argv[2]);
 			ret = nspire_file_read(handle, argv[2], nspire_file_buffer, file_info->size, &file_info->size);
 			if (ret) {
@@ -139,7 +137,7 @@ int main(int argc, char * argv[]) {
 			}
 
 			printf("Saving to %s...\n", argv[3]);
-			fwrite(nspire_file_buffer, file_info->size, 1, get_file);
+			fwrite(nspire_file_buffer, file_info->size, 1, nspire_file);
 			fclose(nspire_file);
 			printf("Saved!\n");
 
